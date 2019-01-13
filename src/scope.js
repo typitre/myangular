76153354a4c6d3437034daf02984b5e9e3c8ce17
typitre <tyrell.pitre@gmail.com>
2019-01-13 08:50:52 -0700

    Initial commit

---
+++ src/scope.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var _ = require('lodash');
+
+function Scope() {
+  this.$$watchers = [];
+}
+
+Scope.prototype.$watch = function(watchFn, listenerFn) {
+  var watcher = {
+    watchFn: watchFn,
+    listenerFn: listenerFn
+  };
+  this.$$watchers.push(watcher);
+};
+
+Scope.prototype.$digest = function() {
+  var self = this;
+  var newValue, oldValue;
+  _.forEach(this.$$watchers, function(watcher) {
+    newValue = watcher.watchFn(self);
+    oldValue = watcher.last;
+    if (newValue !== oldValue) {
+      watcher.last = newValue;
+      watcher.listenerFn(newValue, oldValue, self);
+    }
+  });
+};
+
+module.exports = Scope;
