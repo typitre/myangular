b3ab31db6409c8404f8b883ca6530aa9cf1d4c16
typitre <tyrell.pitre@gmail.com>
2019-01-19 08:29:32 -0700

    value based dirty checking

--- src/scope.js
+++ src/scope.js
@@ -9,10 +9,11 @@ function Scope() {
 
 function initWatchVal() {}
 
-Scope.prototype.$watch = function(watchFn, listenerFn) {
+Scope.prototype.$watch = function(watchFn, listenerFn, valueEq) {
   var watcher = {
     watchFn: watchFn,
     listenerFn: listenerFn || function() {},
+    valueEq: !!valueEq,
     last: initWatchVal
   };
   this.$$watchers.push(watcher);
@@ -25,9 +26,9 @@ Scope.prototype.$$digestOnce = function() {
   _.forEach(this.$$watchers, function(watcher) {
     newValue = watcher.watchFn(self);
     oldValue = watcher.last;
-    if (newValue !== oldValue) {
+    if (!self.$$areEqual(newValue, oldValue, watcher.valueEq)) {
       self.$$lastDirtyWatch = watcher;
-      watcher.last = newValue;
+      watcher.last = (watcher.valueEq ? _.cloneDeep(newValue) : newValue);
       watcher.listenerFn(newValue, 
         (oldValue === initWatchVal ? newValue : oldValue),
         self);
@@ -49,6 +50,14 @@ Scope.prototype.$digest = function() {
       throw '10 digest iterations reaches';
     }
   } while (dirty);
+};
+
+Scope.prototype.$$areEqual = function(newValue, oldValue, valueEq) {
+  if (valueEq) {
+    return _.isEqual(newValue, oldValue);
+  } else {
+    return newValue === oldValue;
+  }
 };
 
 module.exports = Scope;
