b1d36a9714946ee67eb80b7881884fdca271522c
typitre <tyrell.pitre@gmail.com>
2019-01-29 05:45:29 -0700

    running code after a digest - $$postDigest

--- src/scope.js
+++ src/scope.js
@@ -8,6 +8,7 @@ function Scope() {
   this.$$asyncQueue = [];
   this.$$applyAsyncQueue= [];
   this.$$applyAsyncId = null;
+  this.$$postDigestQueue = [];
   this.$$phase = null;
 }
 
@@ -81,6 +82,10 @@ Scope.prototype.$digest = function() {
     }
   } while (dirty || this.$$asyncQueue.length);
   this.$clearPhase();
+
+  while (this.$$postDigestQueue.length) {
+    this.$$postDigestQueue.shift()();
+  }
 };
 
 Scope.prototype.$$areEqual = function(newValue, oldValue, valueEq) {
@@ -146,6 +151,10 @@ Scope.prototype.$$flushApplyAsync = function() {
     this.$$applyAsyncQueue.shift()();
   }
   this.$$applyAsyncId = null;
+};
+
+Scope.prototype.$$postDigest = function(fn) {
+  this.$$postDigestQueue.push(fn);
 };
 
 module.exports = Scope;
