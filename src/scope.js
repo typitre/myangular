98ec6b6b0c6bc0b79213416dc91b54851e897dd5
typitre <tyrell.pitre@gmail.com>
2019-02-07 05:55:12 -0700

    making a child scope

--- src/scope.js
+++ src/scope.js
@@ -216,4 +216,11 @@ Scope.prototype.$watchGroup = function(watchFns, listenerFn) {
   };
 };
 
+Scope.prototype.$new = function() {
+  var ChildScope = function() { };
+  ChildScope.prototype = this;
+  var child = new ChildScope();
+  return child;
+};
+
 module.exports = Scope;
