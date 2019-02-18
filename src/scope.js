e58e7859110f3c66b121dc4e7c458d948cbbb880
typitre <tyrell.pitre@gmail.com>
2019-02-18 09:58:26 -0700

    detecting non-collection changes

--- src/scope.js
+++ src/scope.js
@@ -266,4 +266,28 @@ Scope.prototype.$destroy = function() {
   this.$$watchers = null;
 };
 
+Scope.prototype.$watchCollection = function(watchFn, listenerFn) {
+  var self = this;
+  var newValue;
+  var oldValue;
+  var changeCount = 0;
+
+  var internalWatchFn = function(scope) {
+    newValue = watchFn(scope);
+
+    if (!self.$$areEqual(newValue, oldValue, false)) {
+      changeCount++;
+    }
+    oldValue = newValue;
+
+    return changeCount;
+  };
+
+  var internalListenerFn = function() {
+    listenerFn(newValue, oldValue, self);
+  };
+
+  return this.$watch(internalWatchFn, internalListenerFn);
+};
+
 module.exports = Scope;
